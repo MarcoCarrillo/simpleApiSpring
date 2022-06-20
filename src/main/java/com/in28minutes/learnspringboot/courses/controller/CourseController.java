811fca605673b7ca67e2fbefdd664f9915e07dec
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2022-06-19 20:04:50 -0500

    JPA and H2 db
    Integration of Spring with JPA and memory database H2

--- src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -10,9 +10,16 @@ import com.in28minutes.learnspringboot.courses.bean.Course;
 
 @RestController
 public class CourseController {
-	//localhost:8080/courses
+	//localhost:9090/courses
 	@GetMapping("/courses")
 	public List<Course> getAllCourses() {
-		return Arrays.asList(new Course(1, "Learn Microservices", "Marco Carrillo"));
+		return Arrays.asList(new Course(1, "Learn Microservices", "Marco Carrillo"),
+				new Course(2, "Fullstack React", "i28Minutes"));
+	}
+	
+	//localhost:9090/courses/1
+	@GetMapping("/courses/1")
+	public Course getCourseDetails() {
+		return new Course(1, "Learn Microservices with spring", "Marco Carrillo");
 	}
 }
