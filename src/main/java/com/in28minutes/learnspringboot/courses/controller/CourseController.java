600cfb27cc27bdb6c02cfe4697575c2081b096c4
Marco <marco.carrillo.is@unipolidgo.edu.mx>
2022-06-19 20:45:55 -0500

    getCourseById without hardcode

--- src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
+++ src/main/java/com/in28minutes/learnspringboot/courses/controller/CourseController.java
@@ -2,9 +2,11 @@ package com.in28minutes.learnspringboot.courses.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.learnspringboot.courses.bean.Course;
@@ -26,8 +28,12 @@ public class CourseController {
 	}
 	
 	//localhost:9090/courses/1
-	@GetMapping("/courses/1")
-	public Course getCourseDetails() {
-		return new Course(1, "Learn Microservices with spring", "Marco Carrillo");
+	@GetMapping("/courses/{id}")
+	public Course getCourseDetails(@PathVariable long id) {
+		Optional<Course> course = repository.findById(id);
+		if(course.isEmpty()) {
+			throw new RuntimeException("Course not found with id: " + id);
+		}
+		return course.get();
 	}
 }
